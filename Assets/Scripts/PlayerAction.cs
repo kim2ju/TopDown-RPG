0c4c6be0b9ee3c19cc096eba4e42b533511fc11b
kim2ju <bamtori05@naver.com>
2023-08-27 04:20:22 +0900

    2. 쯔꾸르식 액션 구현하기

--- Assets/Scripts/PlayerAction.cs
+++ Assets/Scripts/PlayerAction.cs
@@ -6,22 +6,81 @@ public class PlayerAction : MonoBehaviour
 {
     float h;
     float v;
+    bool isHorizonMove;
+    Vector3 dirVec;
+    GameObject scanObject;
 
     Rigidbody2D rigid;
+    Animator anim;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
     {
         h = Input.GetAxisRaw("Horizontal");
         v = Input.GetAxisRaw("Vertical");
+
+        bool hDown = Input.GetButtonDown("Horizontal");
+        bool vDown = Input.GetButtonDown("Vertical");
+        bool hUp = Input.GetButtonUp("Horizontal");
+        bool vUp = Input.GetButtonUp("Vertical");
+
+        if (hDown)
+            isHorizonMove = true;
+        else if (vDown)
+            isHorizonMove = false;
+        else if (hUp || vUp)
+            isHorizonMove = h != 0;
+
+        anim.SetBool("isHorizon", isHorizonMove);
+
+        if (anim.GetInteger("hAxisRaw") != h)
+        {
+            anim.SetBool("isChange", true);
+            anim.SetInteger("hAxisRaw", (int)h);
+        }
+        else if (anim.GetInteger("vAxisRaw") != v)
+        {
+            anim.SetBool("isChange", true);
+            anim.SetInteger("vAxisRaw", (int)v);
+        }
+        else
+        {
+            anim.SetBool("isChange", false);
+        }
+
+        if (!isHorizonMove && v == 1)
+            dirVec = Vector3.up;
+        else if (!isHorizonMove && v == -1)
+            dirVec = Vector3.down;
+        else if (isHorizonMove && h == -1)
+            dirVec = Vector3.left;
+        else if (isHorizonMove && h == 1)
+            dirVec = Vector3.right;
+
+        if (Input.GetButtonDown("Jump") && scanObject != null)
+            Debug.Log(scanObject.name);
     }
 
     void FixedUpdate()
     {
-        rigid.velocity = new Vector2(h, v);
+        Vector2 moveVec = isHorizonMove ? new Vector2(h, 0) : new Vector2(0, v);
+        rigid.velocity = moveVec;
+
+        Debug.DrawRay(rigid.position, dirVec * 0.7f, new Color(0, 1, 0));
+        RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, dirVec, 0.7f, LayerMask.GetMask("Object"));
+
+        if (rayHit.collider != null)
+        {
+            scanObject = rayHit.collider.gameObject;
+        }
+        else
+        {
+            scanObject = null;
+        }
     }
 }
